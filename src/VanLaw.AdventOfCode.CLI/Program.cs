b74af4f40a027df7e07622ead2334dc759bd0ada
Chris Van Law <chris@chrisvanlaw.com>
2021-12-02 20:42:12 -0700

    add project scaffold

---
+++ src/VanLaw.AdventOfCode.CLI/Program.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace VanLaw.AdventOfCode.CLI
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+             return await CreateHostBuilder(args)
+                .RunCommandLineApplicationAsync<Program>(args, (app) =>
+                {
+                    app
+                        .GetServices<ICommand>()
+                        .ToList()
+                        .ForEach(c =>
+                        {
+                            c.Configure(app);
+                        });
+                }).ConfigureAwait(false);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.ConfigureCommands();
+                });
+    }
+}
